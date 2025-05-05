a7b7f52bc8e027ee7f4e85946ae093dfc4cea8c3
Prisha Sharma <140067623+parharti@users.noreply.github.com>
2025-05-05 23:29:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -106,6 +106,7 @@ disp(fobj(xo));
 });
 
 // Route for returning Primal Simplex MATLAB code
+// Route for returning Primal Simplex MATLAB code
 app.get('/simplex', (req, res) => {
     const code = `
 % Simplex Method
@@ -118,58 +119,60 @@ clear all
 format short
 Noofvariables=2;
 C=[2 5];
-a=[1 4; 3 1; 1 1]
-b=[24; 21; 9]
-s=eye(size(a,1))
-A=[a s b]
-cost=zeros(1,size(A,2))
-cost(1:Noofvariables)=C
-bv= Noofvariables+1:1:size(A,2)-1
-zjcj=cost(bv)*A-cost
-zcj=[zjcj; A]
+a=[1 4; 3 1; 1 1];
+b=[24; 21; 9];
+s=eye(size(a,1));
+A=[a s b];
+cost=zeros(1,size(A,2));
+cost(1:Noofvariables)=C;
+bv= Noofvariables+1:1:size(A,2)-1;
+zjcj=cost(bv)*A-cost;
+zcj=[zjcj; A];
 simptable=array2table(zcj);
-simptable.Properties.VariableNames(1:size(zcj,2))={'x_1','x_2','s_1','s_2','s_3','sol'}
+simptable.Properties.VariableNames(1:size(zcj,2))={'x_1','x_2','s_1','s_2','s_3','sol'};
 RUN=true;
 while RUN
-if any(zjcj<0); %check for (most) negative value
-    fprintf(' the current BFS is not optimal \n')
-   zc=zjcj(1:end-1);
-   [Enter_val, pvt_col]= min(zc) 
-   if all(A(:,pvt_col)<=0)
-    error('LPP is Unbounded all enteries are <=0 in column %d',pvt_col);
-   else
-       sol=A(:,end)
-       column=A(:,pvt_col)
-       for i=1:size(A,1)
-         if column(i)>0
-            ratio(i)= sol(i)./column(i)
-         else
-            ratio(i)=inf
-         end
-       end
-       [leaving_val, pvt_row]=min(ratio)
-   end
-bv(pvt_row)=pvt_col
-pvt_key=A(pvt_row, pvt_col)
-A(pvt_row,:)=A(pvt_row,:)./pvt_key
-for i=1:size(A,1)
-    if i~=pvt_row
-        A(i,:)=A(i,:)-A(i, pvt_col).*A(pvt_row,:)
+    if any(zjcj<0)
+        fprintf(' the current BFS is not optimal \\n');
+        zc=zjcj(1:end-1);
+        [Enter_val, pvt_col]= min(zc);
+        if all(A(:,pvt_col)<=0)
+            error('LPP is Unbounded all enteries are <=0 in column %d',pvt_col);
+        else
+            sol=A(:,end);
+            column=A(:,pvt_col);
+            for i=1:size(A,1)
+                if column(i)>0
+                    ratio(i)= sol(i)./column(i);
+                else
+                    ratio(i)=inf;
+                end
+            end
+            [leaving_val, pvt_row]=min(ratio);
+        end
+        bv(pvt_row)=pvt_col;
+        pvt_key=A(pvt_row, pvt_col);
+        A(pvt_row,:)=A(pvt_row,:)./pvt_key;
+        for i=1:size(A,1)
+            if i~=pvt_row
+                A(i,:)=A(i,:)-A(i, pvt_col).*A(pvt_row,:);
+            end
+        end
+        zjcj=zjcj-zjcj(pvt_col).*A(pvt_row,:);
+        zcj=[zjcj;A];
+        table=array2table(zcj);
+        table.Properties.VariableNames(1:size(zcj,2))={'x_1','x_2','s_1','s_2','s_3','sol'};
+    else
+        RUN=false;
+        fprintf('The current BFS is optimal \\n');
     end
 end
-    zjcj=zjcj-zjcj(pvt_col).*A(pvt_row,:)
-    zcj=[zjcj;A]
-    table=array2table(zcj)
-    table.Properties.VariableNames(1:size(zcj,2))={'x_1','x_2','s_1','s_2','s_3','sol'}
-else
-    RUN=false;
-    fprintf('The current BFS is optimal \n')
-end
-end
-;
+`;
     res.type('text/plain').send(code);
 });
 
+
+// Route for returning Dual Simplex MATLAB code
 // Route for returning Dual Simplex MATLAB code
 app.get('/dual', (req, res) => {
     const code = `
@@ -177,42 +180,43 @@ format short
 clc
 clear all
 
-C=[-2 0 -1 0 0 0]
-A=[-1 -1 1 1 0 -5; -1 2 -4 0 1 -8]
-ib=[4 5]
-zjcj=C(ib)*A-C
+C=[-2 0 -1 0 0 0];
+A=[-1 -1 1 1 0 -5; -1 2 -4 0 1 -8];
+ib=[4 5];
+zjcj=C(ib)*A-C;
 RUN=true;
 while RUN
     if any(A(:,size(A,2))<0)
-        fprintf('the current BFS is not feasible')
-        [lvg_val, pvt_row]=min(A(:,size(A,2)))
-for i=1:size(A,2)-1
-    if A(pvt_row,i)<0
-        m(i)=zjcj(i)/A(pvt_row,i)
-    else
-         m(i)=-inf
-     end
-end
-[ent_val, pvt_col]=max(m)
-A(pvt_row,:)=A(pvt_row,:)/A(pvt_row,pvt_col)
-for i=1:size(A,1)
-     if i~=pvt_row
-         A(i,:)=A(i,:)-A(i,pvt_col).*A(pvt_row,:)
-     end 
-end
-ib(pvt_row)=pvt_col;
-zjcj=zjcj-zjcj(pvt_col).*A(pvt_row,:)
-ZCj=[zjcj;A]
+        fprintf('the current BFS is not feasible\\n');
+        [lvg_val, pvt_row]=min(A(:,size(A,2)));
+        for i=1:size(A,2)-1
+            if A(pvt_row,i)<0
+                m(i)=zjcj(i)/A(pvt_row,i);
+            else
+                m(i)=-inf;
+            end
+        end
+        [ent_val, pvt_col]=max(m);
+        A(pvt_row,:)=A(pvt_row,:)/A(pvt_row,pvt_col);
+        for i=1:size(A,1)
+            if i~=pvt_row
+                A(i,:)=A(i,:)-A(i,pvt_col).*A(pvt_row,:);
+            end 
+        end
+        ib(pvt_row)=pvt_col;
+        zjcj=zjcj-zjcj(pvt_col).*A(pvt_row,:);
+        ZCj=[zjcj;A];
         TABLE=array2table(ZCj);
-        TABLE.Properties.VariableNames(1:size(ZCj,2))={'x_1','x_2','x3','s_1','s_2','sol'}
-else
-    RUN=false;
- fprintf('    current BFS is Feasible and Optimal ')
-end
+        TABLE.Properties.VariableNames(1:size(ZCj,2))={'x_1','x_2','x3','s_1','s_2','sol'};
+    else
+        RUN=false;
+        fprintf('current BFS is Feasible and Optimal\\n');
+    end
 end
 `;
     res.type('text/plain').send(code);
 });
+
 
 // Start the server
 app.listen(port, () => {
